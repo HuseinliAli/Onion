baf26f75994807300e085231e5f01494e3e3ae63
HuseinliAli <ali.guseynoov@gmail.com>
2024-01-04 02:03:19 +0400

    Project Configuration

--- API/Program.cs
+++ API/Program.cs
@@ -28,45 +28,6 @@ app.UseForwardedHeaders(new()
 app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
-app.Use(async (context, next) =>
-{
-    Console.WriteLine("Login before executing the next delegate in the Use method");
-    await next.Invoke();
-    Console.WriteLine("Login after executing the next delegate in the Use method");
-
-});
-
-app.Map("/usingmapbranch", builder =>
-{
-    builder.Use(async (context, next) =>
-    {
-        Console.WriteLine("Map branch logic in the Use method before the next delegate");
-        await next.Invoke();
-        Console.WriteLine("Map branch logic in the Use method after the next delegate");
-    });
-
-    builder.Run(async context =>
-    {
-        Console.WriteLine("Map branch response to the client in the Run method");
-        await context.Response.WriteAsync("Hello from the map branch");
-
-    });
-});
-
-app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), builder
-    =>
-{
-    builder.Run(async context =>
-    {
-        await context.Response.WriteAsync("Hello from MapWhen branch");
-    });
-});
-
-app.Run(async context =>
-{
-    Console.WriteLine("Writing the response to the client in the Run method");
-    await context.Response.WriteAsync("Hello from middleware component");
-});
 
 app.MapControllers();
 
